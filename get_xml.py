1ca6782d3ead39fb1739aec16b42cbf7efd200c2
Milosz Roszak <m.roszak@vector.net>
2023-02-05 19:15:43 +0100

    Created csv of usd courses

--- get_xml.py
+++ get_xml.py
@@ -1,6 +1,8 @@
 import requests
 import xml.etree.ElementTree as ET
 from data import work_days_calendar
+import csv
+import pandas as pd
 
 kursy_dolara = []
 table_no = "001"
@@ -22,13 +24,27 @@ for tab in range(1, 507):
         else:
             URL_template = f"https://static.nbp.pl/dane/kursy/xml/a{tab-254}z{valid_calendar[tab-1]}.xml"       
 
-# URL_template = f"https://static.nbp.pl/dane/kursy/xml/a{table_no}z{date_no}.xml"
-    response = requests.get(URL_template)
-    with open(f'data/{tab}.xml', 'wb') as f:
-        f.write(response.content)
+# URL_template = f"https://static.nbp.pl/dane/kursy/xml/a{table_no}z{date_no}.xml"4.4100
+    # response = requests.get(URL_template)
+    # with open(f'data/{tab}.xml', 'wb') as f:
+    #     f.write(response.content)
     tree = ET.parse(f'data/{tab}.xml')
     root = tree.getroot()
     usd1 = root[3][3].text
-    kursy_dolara.append(usd1)
+    usd1_dot = usd1.replace(',', '.')
+    kursy_dolara.append(usd1_dot)
+    header = ['USD to PLN']
     # print(root[3][3].text)
 print(f">>>{kursy_dolara}")
+
+with open("USD_PLN_courses.txt", "w") as f:
+    for i in kursy_dolara:
+        f.write(i)
+        f.write(', ')
+
+kursy_dolara_float = []
+for i in kursy_dolara:
+    kursy_dolara_float.append(float(i))
+
+df = pd.DataFrame(kursy_dolara_float)
+df.to_csv("file2.csv", index=False, header=False)
